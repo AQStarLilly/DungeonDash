53e99920d80f6d36d89d506d0df3266753d47653
Kieran Hislop <kieranjhislop@gmail.com>
2025-10-06 11:59:44 -0300

    added temp controls info & psuedocode for Sound/Upgrades

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //SFX - 10(Minimum)
+    //Player
+        //Death (plays and goes into the results screen)
+        //Attack
+        //Take Damage
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Enemy
+        //Death
+        //Attack
+        //Take Damage
+
+    //Boss
+        //Some sort of line for the boss to say or have the boss howl/yell as you come up to them?
+        //Death
+        //Attack
+        //Take Damage
+
+    //DUNGEON DASH - play when you first load into the game - and when you hit dash again? (just yell dungeon dash with intense feel)
+    //Upgrade Button SFX?
+    //Game Won SFX - plays when you beat the final boss
+    
+
+
+    //Music - 2(Minimum)
+    //Main Menu - Plays in Main menu
+    //Gameplay - Plays in Gameplay, PauseMenu (maybe gets more intense when you go to fight the boss / new music track for Boss Fight)
+    //Upgrades Menu - Casual Shop-Like Music
+    //Game Won Music ? 
 }
--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -3,16 +3,30 @@ using UnityEngine;
 public class UpgradeManager : MonoBehaviour
 {
     //Set up all upgrade references - Dictionary?
-    
-    
-    //loop to check gold vs how much you need per upgrade
+
+
+    //loop to check gold vs how much you need per upgrade?
     //if you don't have enough button should be greyed out and if you try to click on it nothing will happen or maybe the button or currency will flash red
     //Upgrades you can buy will be colored, like green or something to show you can afford it
     //Have to update currency so when you buy upgrades it removes that currency from your total
-    
-    
+
+
     //Connect upgrades to player / gameplay so they affect him or spawn allies in scene (added health and damage from allies or ability you can use?)
-    
-    
+
+
     //Add the clear option when starting a new game / wipes all upgrades.
+
+    //Upgrades - Set value each upgrade (+10) or just a multiplier (*2) - Probably multiplier (fix enemies to match if I do this one)
+                                                                            //also try to not have multiplier be to crazy, like health increasing by 100 or more
+    //Balance is hard, both enemy and player need to scale similarly so game feels smooth and not a mess ((sudden increase in stats)should be more gradual)
+    //Game takes 30 mins or so ? Don't want to take to long or be to fast. May have to tweak number of waves depending on balance and multipliers
+
+    //Damage 1
+    //Damage 2
+    //Health
+    //Shield (Health Bar 2)
+    //Currency Multiplier
+    //Summonable Ally 1 (Add health & Damage to a collective pool  ||  Ability you gain from them (Janitor may throw buckets at people, 10 second cooldown)
+    //Summonable Ally 2 (Add health & Damage to a collective pool  ||  Ability you gain from them (Janitor may throw buckets at people, 10 second cooldown)
+    //Summonable Ally 3 (Add health & Damage to a collective pool  ||  Ability you gain from them (Janitor may throw buckets at people, 10 second cooldown)
 }
